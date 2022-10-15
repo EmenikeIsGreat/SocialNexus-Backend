30c8166aca96e4f038d940325a18a27ecb82df71
EmenikeIsGreat <emenikeani3@gmail.com>
2022-10-15 17:59:27 -0400

    fixing

--- FrontEndToBackEndActions/settings/changeName.js
+++ FrontEndToBackEndActions/settings/changeName.js
@@ -2,21 +2,21 @@ const axios = require('axios')
 
 
 
-const nameJson = {
-    id:'62c0eadc47ec21fd9e585023',
-    firstName:"Joe",
-    lastName:"Shiesty"
-}
+// const nameJson = {
+//     id:'634592c92ef1440c9a4f9452',
+//     firstName:"Joe",
+//     lastName:"Shiesty"
+// }
 
 
 
 async function changeName(nameJson){
-    const res = await axios.post('http://localhost:3000/settings/changeName',nameJson)
+    const res = await axios.post('http://localhost:8080/settings/changeName',nameJson)
     console.log(res.data)
 }
 
 
-//changeName(nameJson)
+changeName(nameJson)
 
 async function test(){
     const res = await axios.get('http://localhost:3000/test')
