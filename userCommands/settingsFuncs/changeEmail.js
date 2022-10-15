28da9633b06b00ac37d9e01b5829a8f4bd6f05c4
EmenikeIsGreat <emenikeani3@gmail.com>
2022-10-15 18:23:07 -0400

    testing

--- userCommands/settingsFuncs/changeEmail.js
+++ userCommands/settingsFuncs/changeEmail.js
@@ -1,4 +1,4 @@
-"const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const url = "mongodb+srv://Emenike:Ninjaboy12345$@cluster0.lc7v34m.mongodb.net/?retryWrites=true&w=majority"
 const user = require('../../schemas/User')
 const checkIfEmailExist = require('../checks/checkEmailExist')
