8ef31c8b09f59517497c0631c8198e6f59cec695
EmenikeIsGreat <emenikeani3@gmail.com>
2022-11-26 23:06:55 -0500

    testing

--- routes/user.js
+++ routes/user.js
@@ -96,7 +96,7 @@ router.get('/queryUser', (req, res) =>{
 
 router.get('/deletePhoto:id', async (req, res) =>{
 
-    let user = userSchema.findById(req.params.id)
+    let user = await userSchema.findById(req.params.id)
     console.log(user)
     if(user.hasProfilePic){
         let readStream = deletePhoto(req.params.id)
