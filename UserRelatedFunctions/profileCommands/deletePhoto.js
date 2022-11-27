b2caafc970504f4703f64b4ce1970048ccee3629
EmenikeIsGreat <emenikeani3@gmail.com>
2022-11-27 00:09:30 -0500

    testing

--- UserRelatedFunctions/profileCommands/deletePhoto.js
+++ UserRelatedFunctions/profileCommands/deletePhoto.js
@@ -5,7 +5,7 @@ const path = require('path');
 const coolPath = path.join(__dirname, '../../.env')
 require("dotenv").config({path:coolPath})
 
-console.log(coolPath)
+
 const fs = require('fs')
 const S3 = require('aws-sdk/clients/s3')
 
@@ -19,6 +19,7 @@ const secretAccessKey = process.env.AWS_SECRET_KEY
 
 
 async function deletePhoto(key){
+  console.log("YOOOOOOOO")
     const s3 = new S3({
         region,
         accessKeyId,
@@ -30,14 +31,17 @@ async function deletePhoto(key){
           Key:key 
         };
         
-        s3.deleteObject(params, function(err, data) {
+        await s3.deleteObject(params, function(err, data) {
           console.log(data)
-        });
+        }).promise()
+
+
+
 }
 
 
 
-//deletePhoto("Whatever")
+//deletePhoto("6382725ccf26091d21536496")
 
 exports.deletePhoto = deletePhoto
 
