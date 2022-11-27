4271bc80ed2a435f7a6581c090b8c746a256fd97
EmenikeIsGreat <emenikeani3@gmail.com>
2022-11-27 17:02:46 -0500

    testing

--- routes/transaction.js
+++ routes/transaction.js
@@ -144,7 +144,7 @@ router.get('/conversion', async (req,res) =>{
 
 // need to check on this to see if it works
 router.post('/HandleEvent', async (req,res) =>{
-    console.log(req.body[0].type)
+    console.log(req.body)
     await eventHandler(req.body)
 })
 
