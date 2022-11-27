b549e150c4f415aae65bd535d4cf480bee7aa9e7
EmenikeIsGreat <emenikeani3@gmail.com>
2022-11-26 23:14:24 -0500

    tesdting

--- routes/user.js
+++ routes/user.js
@@ -96,7 +96,7 @@ router.get('/queryUser', (req, res) =>{
 
 router.get('/deletePhoto', async (req, res) =>{
 
-    console.log(req.params.id)
+    console.log(req.params)
     let user = await userSchema.findById(req.params.id)
     console.log(user)
     if(user.hasProfilePic){
