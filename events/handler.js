efb94e122166910db23ca49bf1cadbf2f47ed6d8
EmenikeIsGreat <emenikeani3@gmail.com>
2022-12-23 00:22:27 -0500

    cec

---
+++ events/handler.js
@@ -0,0 +1,55 @@
+/*
+
+this watches the messages databse and sends notifcation to user in real time about their messages
+
+*/
+
+
+const {MongoClient} = require('mongodb')
+const Pusher = require("pusher");
+const path = require('path');
+const pushNotification = require('./pushNotification')
+
+const coolPath = path.join(__dirname, '../.env')
+require("dotenv").config({path:coolPath})
+
+const client = new MongoClient(process.env.MONGODB_URL)
+
+
+//handles notification to users
+async function pushTransactionEvents(){
+
+    
+    try{
+
+        await client.connect()
+        console.log("Message event handler is deployed")
+
+        let db1 = client.db("test").collection("messages")
+        
+
+        const changeStreamIterator = db1.watch();
+
+
+        while(await changeStreamIterator.hasNext()){
+
+            const next = await changeStreamIterator.next();
+
+
+            pusher.trigger("testing", next.fullDocument.recipient, {
+            message: next.fullDocument.body
+            });
+
+            pushNotification(next.fullDocument.recipient,"notification",next.fullDocument.body)
+        }
+
+    }
+    catch(error){
+        console.log(error);
+        return error
+    }
+}
+
+//pushTransactionEvents()
+
+
---
+++ events/pushNotification.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+const path = require('path');
+
+
+
+const coolPath = path.join(__dirname, '../.env')
+require("dotenv").config({path:coolPath})
+
+
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(8080, () => {
+  console.log('listening on *:3000');
+});
+
+const sendMessage = (message) => {
+  io.emit('message', message);
+}
+
