badd077114c78257f0e4c7ac431d77a57dd2e01d
EmenikeIsGreat <emenikeani3@gmail.com>
2022-12-23 19:15:21 -0500

    tsting

--- events/pushNotification.js
+++ events/pushNotification.js
@@ -1,28 +1,18 @@
-const axios = require('axios')
-const path = require('path');
+const http = require('http');
+const socket = require("socket.io")
+const httpServer = http.createServer();
 
 
 
-const coolPath = path.join(__dirname, '../.env')
-require("dotenv").config({path:coolPath})
+const Server = require("socket.io")
 
-
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-
-io.on('connection', socket => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+const io = new Server.Server(httpServer, {
 });
 
-http.listen(8080, () => {
-  console.log('listening on *:8080');
-});
+setInterval(()=>{
+   io.sockets.emit("hi", "everyone");
+   console.log("Emenike")
+},2000)
 
-const sendMessage = (message) => {
-  io.emit('message', message);
-}
+
+httpServer.listen(8080);
