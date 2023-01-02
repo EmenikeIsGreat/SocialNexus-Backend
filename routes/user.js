a6c9867fcd8fed6a79c10d2b6c245d1d9bf71181
EmenikeIsGreat <emenikeani3@gmail.com>
2023-01-02 15:15:25 -0500

    upload testing

--- routes/user.js
+++ routes/user.js
@@ -129,7 +129,7 @@ router.get('/deletePhoto', async (req, res) =>{
 router.post('/changePhoto:id', upload.single('profile'), async (req, res) =>{
     console.log("changing photo")
     // console.log("this is the ID 1" + req.params.id)
-    // console.log("this is the ID 2" + req.query.id)
+    console.log("this is the ID 2" + req.query.id)
 
 
     
