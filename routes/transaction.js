43e7dfb09caa3619c86065bdf8312ae2df5ee1d0
EmenikeIsGreat <emenikeani3@gmail.com>
2023-01-06 18:25:54 -0500

    testing

--- routes/transaction.js
+++ routes/transaction.js
@@ -146,7 +146,8 @@ router.get('/conversion', async (req,res) =>{
 router.post('/HandleEvent', async (req,res) =>{
 
     console.log("-------------PROCESSING BLOCKCHAIN EVENTS--------------")
-    console.log(req.body)
+    console.log(typeof(req.body))
+    console.log(req.body[0].payload)
     await eventHandler(req.body)
     res.end()
 })
