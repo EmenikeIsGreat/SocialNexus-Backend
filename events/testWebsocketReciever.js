3258ca442f4f4a876f04e7d7d8d7ff291f94116b
EmenikeIsGreat <emenikeani3@gmail.com>
2023-01-06 18:53:31 -0500

    fixing

--- events/testWebsocketReciever.js
+++ events/testWebsocketReciever.js
@@ -1,7 +1,7 @@
 const socketIO = require("socket.io-client");
 
 
-const socket = socketIO.connect("http://44.203.95.162:80810");
+const socket = socketIO.connect("http://44.203.95.162:8080");
 socket.on("testing", (...args) => {
     console.log(...args);
   });
