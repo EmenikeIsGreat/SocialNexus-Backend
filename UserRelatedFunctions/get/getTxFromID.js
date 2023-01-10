68deabb5b31763a9ab16da85d8e3475fa1ed4848
EmenikeIsGreat <emenikeani3@gmail.com>
2023-01-09 23:00:05 -0500

    testing

--- UserRelatedFunctions/get/getTxFromID.js
+++ UserRelatedFunctions/get/getTxFromID.js
@@ -4,7 +4,6 @@ const query = require('../../Blockchain/wrappedFabConnect/query')
 module.exports = async function getTxFromID(id){
 
     let value = await query("get", [id])
-    console.log(value.result)
     return value.result
 
 
